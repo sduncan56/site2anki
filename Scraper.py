42cac69d5f319aa7a9d033ac96ce25f29d9e70f0
Shane <shane@gmail.com>
2017-10-28 23:55:19 +0100

    scraping mostly done

---
+++ Scraper.py
@@ -0,0 +1,50 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+class Scraper:
+    def __init__(self):
+        pass
+
+    def parseInnerUrl(self, url):
+        with urllib.request.urlopen(url) as url:
+            page = url.read()
+            soup = BeautifulSoup(page, 'html.parser')
+
+            header = soup.find('div', attrs={'class': 'panel-heading'}).find('h1').string
+            answersCollection = soup.find('div', attrs={'class': 'articleContent'}).children
+
+            answer = ""
+
+            correctAnswer = soup.find('a', attrs={'title': 'Toggle display correct answer'})
+            if correctAnswer:
+                js = correctAnswer['href']
+                index = int(js.find('getElementById') + 16)
+                index2 = int(js[index:].find("')"))
+                answerTag = js[index:index + index2]
+                answer = soup.find('li', attrs={'id': answerTag}).text
+            else:
+                answer = soup.find('div', attrs={'class': 'articleContent'}).find('div').text
+
+
+            return (header, answer)
+
+    def scrape(self, baseurl, pageurl):
+        with urllib.request.urlopen(baseurl+pageurl) as url:
+            page = url.read()
+            soup = BeautifulSoup(page, 'html.parser')
+            innerUrlsHtml = soup.findAll('div', attrs={'class': 'list-group-item-spacer'})
+
+            for innerUrlHtml in innerUrlsHtml:
+                innerUrl = innerUrlHtml.find('a')['href']
+                data = self.parseInnerUrl(baseurl+innerUrl)
+                print(data)
+
+
+
+
+
+
+if __name__ == "__main__":
+    scraper = Scraper()
+    scraper.scrape("http://www.dotnetfunda.com", "/interviews/exclusive/cat/6/csharp")
